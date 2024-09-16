269063f37f1510fda16e275c26e44f69d07d623f
Maria Fernanda Reis <00001107533375sp@al.educacao.sp.gov.br>
2024-09-16 11:55:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,19 +3,129 @@ function setup() {
 }
 
 function draw() {
-  background(220);     
+  background(220);
 }
 function setup() {
-  createCanvas(400, 400);
-  background("white")
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("rgb(87,45,102)");
+}
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("#8C369B");
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("#652A6F");
+    rect(0, 0, 100, 150)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+    fill("red")
+    rect(0, 0, 100, 150)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+    stroke("blue")
+    fill("red")
+    rect(0, 0, 100, 150)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+  stroke("blue")
+  fill("red")
+  rect(100, 250, 100, 150)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+  stroke("blue")
+  fill("red")
+  rect(mouseX, mouseY, 20, 35)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+  stroke("blue")
+  fill("red")
+  
+  console.log(mouseIsPressed)
+  rect(mouseX, mouseY, 20, 35)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+  stroke("blue")
+  fill("red")
+  
+  // console.log(mouseIsPressed)
+  rect(mouseX, mouseY, 20, 35)
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+  stroke("blue")
+  fill("red")
+  
+  // console.log(mouseIsPressed)
+  
+  if(mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35)
+  } 
+}
+function setup() {
+  createCanvas(600, 600);
+  background("white");
 }
 
 function draw() {
   stroke("blue");
   fill("red");
-  
-  
+
+  // console.log(mouseIsPressed)
+
   if (mouseIsPressed) {
     rect(mouseX, mouseY, 20, 35);
   }
 }
+
